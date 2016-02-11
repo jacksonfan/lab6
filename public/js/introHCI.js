a331e5a97b02d5f7581f5d45fb1e0113cbdc372d
Jackson Fan <jhfan@ucsd.edu>
2016-02-11 03:19:03 -0800

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,9 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+   console.log("project/" + idNumber);
+   $.get("project/" + idNumber, addProject);
 }
 
 /*
@@ -35,4 +38,25 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+   $.get("palette", changePalette);
+}
+
+function changePalette(result) {
+   console.log(result);
+   var colors = result['colors']['hex'];
+   $('body').css('background-color', colors[0]);
+   $('.thumbnail').css('background-color', colors[1]);
+   $('h1, h2, h3, h4, h5').css('color', colors[2]);
+   $('p').css('color', colors[3]);
+   $('.project img').css('opacity', 0.75);
+}
+
+function addProject(result) {
+   console.log(result);
+   var projectHTML = "<a href='#' class='thumbnail'>" +
+      "<img src='" + result['image'] + "'class = 'img'>" +
+      "<p>" + result['title'] + "</p>" +
+      "<p><small>" + result['date'] + "</small></p>" + 
+      result['summary'] + "</a>";
+   $("#project" + result['id'] + " .details").html(projectHTML);
 }
